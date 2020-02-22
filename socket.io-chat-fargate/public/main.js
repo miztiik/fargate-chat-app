b7f5f48de8c70acfcf758861277b51fa1aa1ea5e
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-22 23:40:54 +0100

    FixedWelcome

--- socket.io-chat-fargate/public/main.js
+++ socket.io-chat-fargate/public/main.js
@@ -229,7 +229,7 @@ $(function () {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Mystique Chat 💬";
+    var message = "Welcome to Mystique Chat";
     log(message, {
       prepend: true
     });
